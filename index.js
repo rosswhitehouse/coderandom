137ab2bd41104cf7ee1ad8032e6bac98f470f670
Ross Whitehouse <ross.dw@live.com>
2021-03-08 22:19:25 +0000

    Add port from env file.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + '/public/'));
 
